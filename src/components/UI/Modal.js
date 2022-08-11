bd27a02e448f33419b1a8c868af48225d33613b1
Bram Mathijssen <Bram.mathijssen@gmail.com>
2022-08-11 19:27:13 +0200

    finished styling the modal window

--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -6,36 +6,39 @@ import './Modal.scss';
 const Modal = (props) => {
     const cartCtx = useContext(CartContext);
 
-
-
     return (
         <div className="modal">
             <div className="modal__content">
-                <h3 className="modal__title">Modal Window</h3>
                 {cartCtx.cartState.items.map((item) => {
                     return (
 
                         <div className="item" key={item.id}>
                             <div className="item__content">
-                                <p>{item.name}</p>
-                                <p>{item.price}</p>
-                                <p>{item.amount}</p>
+                                <p className="item__name">{item.name}</p>
+                                <div className="item__flexcontainer">
+                                    <p className="item__price">${item.price}</p>
+                                    <p className="item__amount">x {item.amount}</p>
+                                </div>
+
                             </div>
                             <div className="item__controls">
-                                <button className="btn" onClick={() => { cartCtx.addToCart(item, 1); }}>+</button>
-                                <button className="btn" onClick={() => { cartCtx.removeFromCart(item, 1); }}>-</button>
+                                <button className="btn btn--modify" onClick={() => { cartCtx.addToCart(item, 1); }}>+</button>
+                                <button className="btn btn--modify" onClick={() => { cartCtx.removeFromCart(item, 1); }}>-</button>
                             </div>
                         </div>
                     )
                 })}
-                <div className="total">
-                    <p>Total Amount: {cartCtx.totalPrice}</p>
+                <div className="bottom">
+                    <div className="bottom__total">
+                        <p>Total : ${cartCtx.totalPrice.toFixed(2)}</p>
+                    </div>
+                    <div className="bottom__controls">
+                        <button className="btn btn--close" onClick={props.closeModal}>close</button>
+                        <button className="btn btn--order" onClick={props.closeModal}>Order</button>
+                    </div>
                 </div>
-                <button className="btn" onClick={props.closeModal}>close</button>
             </div>
-
         </div>
-
     )
 }
 
