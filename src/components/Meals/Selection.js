d198a5ce7e99337933f74f081c2299735a6e5d24
Bram Mathijssen <Bram.mathijssen@gmail.com>
2022-08-23 02:53:16 +0200

    stored meals on firebase API and fetching the data

--- src/components/Meals/Selection.js
+++ src/components/Meals/Selection.js
@@ -1,12 +1,41 @@
+import { useEffect, useState } from 'react';
 import Meal from './Meal';
 import './Selection.scss';
 
 const Selection = (props) => {
+    const [meals, setMeals] = useState([]);
+
+    useEffect(() => {
+        const fetchMeals = async () => {
+            const response = await fetch('https://react-meals-a793d-default-rtdb.europe-west1.firebasedatabase.app/meals.json');
+            const responseData = await response.json();
+            const loadedMeals = [];
+
+            for (const key in responseData) {
+                try {
+                    loadedMeals.push({
+                        id: key,
+                        name: responseData[key].name,
+                        description: responseData[key].description,
+                        price: responseData[key].price,
+                        src: responseData[key].src
+                    })
+                } catch (e) {
+                    console.log(e);
+                }
+            }
+            setMeals(loadedMeals);
+        }
+        fetchMeals()
+    }, [])
+
+
+
     return (
         <section className='selection'>
             <h2 className='selection__title'>Our Selection</h2>
             <div className='grid-container'>
-                {props.mealData.map((item => <Meal  key={item.id} id={item.id} source={item.src} alt={item.name} title={item.name} description={item.description} price={item.price}/>))}
+                {meals.map((item => <Meal key={item.id} id={item.id} source={item.src} alt={item.name} title={item.name} description={item.description} price={item.price} />))}
             </div>
         </section>
     )
