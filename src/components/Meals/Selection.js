12f5d38ecd5f4b9cf107ac993bb5c171910f828d
Bram Mathijssen <Bram.mathijssen@gmail.com>
2022-08-23 21:54:49 +0200

    implemented Skeleton Loading

--- src/components/Meals/Selection.js
+++ src/components/Meals/Selection.js
@@ -1,9 +1,13 @@
 import { useEffect, useState } from 'react';
 import Meal from './Meal';
 import './Selection.scss';
+import 'react-loading-skeleton/dist/skeleton.css'
+import CardSkeleton from '../UI/CardSkeleton';
+
 
 const Selection = (props) => {
     const [meals, setMeals] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
         const fetchMeals = async () => {
@@ -24,17 +28,22 @@ const Selection = (props) => {
                     console.log(e);
                 }
             }
-            setMeals(loadedMeals);
+
+            // Timer just for demonstrating Skeleton Loading
+            const timer = setTimeout(() => {
+                setMeals(loadedMeals);
+                setIsLoading(false);
+            }, 2000);
+            return () => clearTimeout(timer);
         }
         fetchMeals()
     }, [])
-
-
 
     return (
         <section className='selection'>
             <h2 className='selection__title'>Our Selection</h2>
             <div className='grid-container'>
+                {isLoading && <CardSkeleton cards={6} />}
                 {meals.map((item => <Meal key={item.id} id={item.id} source={item.src} alt={item.name} title={item.name} description={item.description} price={item.price} />))}
             </div>
         </section>
